2c43d931a8c4c03c8383c679444de3f862ae8887
Konstantinos Rigos <rigosk001@gmail.com>
2020-11-25 12:18:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.sidebar.markdown('<p id="my-footer">Made by <a  href="https://www.google.com"
 """
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Edit `/streamlit_app.py` to customize this app to your heart's :clock: desire :heart:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
